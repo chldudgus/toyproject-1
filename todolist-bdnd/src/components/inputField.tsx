9d0b065718ea81f3985a03c14f9f09c0832aba3a
haleychoi <chldudgus617@gmail.com>
2022-10-28 01:04:37 +0900

    first commit

---
+++ todolist-bdnd/src/components/inputField.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "../styles/style.scss";
+import { FiPlus } from "react-icons/fi";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  setPriority: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({
+  todo,
+  setTodo,
+  setPriority,
+  handleAdd
+}) => {
+  return (
+    <div className="u-container -m1">
+      <form className="add-form" onSubmit={handleAdd}>
+        <select
+          className="priorityselect"
+          defaultValue="low"
+          onChange={(e) => setPriority(e.target.value)}
+        >
+          <option disabled value="low">
+            Select priority
+          </option>
+          <option value="high">High</option>
+          <option value="medium">Medium</option>
+          <option value="low">Low</option>
+        </select>
+        <input
+          className="contentinput"
+          type="text"
+          placeholder="Add a task"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button className="submitbtn">
+          <FiPlus />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default InputField;
